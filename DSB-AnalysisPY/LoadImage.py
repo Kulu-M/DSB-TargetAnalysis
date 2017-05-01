00fd61f61da9b444bd5dc44b8ae5bb7e2d728ee3
Kulu-M <lloyd.niebel@googlemail.com>
2017-05-01 16:09:38 +0200

    New Version

--- DSB-AnalysisPY/LoadImage.py
+++ DSB-AnalysisPY/LoadImage.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 # Get the argument which was parsed
 # ap = argparse.ArgumentParser()
@@ -8,7 +9,15 @@ import cv2
 # args = vars(ap.parse_args())
 
 # Load an color image in grayscale
-img = cv2.imread('D:/GITHUB REPOS/PyEdge/123.jpg',0)
+img = cv2.imread('D:/GITHUB REPOS/DSB-TargetAnalysis/DSB-AnalysisPY/test.jpg',0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
+
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
